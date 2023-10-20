460e0da85ec460678c2fc8b79ee9557621e392be
satler-git <mitsclatw24@mbr.nifty.com>
2023-10-20 19:42:02 +0900

    Add sphere render

--- src/Ray.cr
+++ src/Ray.cr
@@ -15,11 +15,11 @@ class Ray
   end
 
   def origin : Point3
-    orig
+    @orig
   end
 
   def direction : Vec3
-    dir
+    @dir
   end
 
   # 位置を計算
@@ -29,7 +29,20 @@ class Ray
 end
 
 def ray_color(r : Ray) : Color
+  if hit_sphere(Point3.new(0, 0, -1), 0.5, r)
+    return Color.new(1, 0, 0)
+  end
+
   unit_direction : Vec3 = unit_vector(r.direction)
   t = 0.5*(unit_direction.y + 1.0)
   (1.0 - t)*Color.new(1.0, 1.0, 1.0) + t*Color.new(0.5, 0.7, 1.0)
+end
+
+def hit_sphere(center : Point3, radius : Float64, r : Ray) : Bool
+  oc : Vec3 = r.origin - center
+  a = dot(r.direction, r.direction)
+  b = 2.0 * dot(oc, r.direction)
+  c = dot(oc, oc) - radius*radius
+  discriminant = b*b - 4*a*c
+  discriminant > 0
 end
